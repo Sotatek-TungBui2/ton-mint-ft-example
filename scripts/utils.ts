0e6402cca5407452239644c84f7a343fef942454
Sotatek-TungBui2 <tung.bui2@sotatek.com>
2024-06-19 10:12:35 +0700

    migrate to new ton verson

---
+++ scripts/utils.ts
@@ -0,0 +1,21 @@
+import { UIProvider, sleep } from "@ton/blueprint";
+
+export const waitForStateChange = async <T>(ui: UIProvider, cb: () => Promise<T>, maxRetries = 20): Promise<T> => {
+    ui.write('Waiting for state change...');
+
+    const stateBefore = await cb();
+    let stateAfter = stateBefore;
+    let attempt = 1;
+    while (stateAfter === stateBefore) {
+        if (attempt > maxRetries) {
+            ui.write('Max retries exceeded');
+            break;
+        }
+        ui.setActionPrompt(`Attempt ${attempt}\n`);
+        await sleep(2000);
+        stateAfter = await cb();
+        attempt++;
+    }
+    ui.clearActionPrompt();
+    return stateAfter;
+}
