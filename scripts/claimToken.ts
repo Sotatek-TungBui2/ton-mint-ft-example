abd88bdc45926febb219c8a0da8892aa6214ffc1
Sotatek-TungBui2 <tung.bui2@sotatek.com>
2024-06-26 08:43:35 +0700

    demo code claim

--- scripts/claimToken.ts
+++ scripts/claimToken.ts
@@ -8,19 +8,22 @@ import { waitForStateChange } from './utils';
 
 export async function run(provider: NetworkProvider) {
     // suppose that you have the cell in base64 form stored somewhere
-    const dictCell = Cell.fromBase64(
-        'te6cckEBBQEAhgACA8/oAgEATUgBXkdqCILh0PQ2sSrNftE0+xRx9GR+xJncvd79fC43/OKLLQXgEAIBIAQDAE0gAVeQTKvOEe38u3LKXrFWkd6VtAQPGeSKLMmk7CBMeBFKO5rKAEAATSAFNj+mB00w0+rZcvP1mD3Voj6fLT6a44VUskxi2FOR1mIdzWUAQBrhY8Y='
-    );
-    const dict = dictCell.beginParse().loadDictDirect(Dictionary.Keys.BigUint(256), claimMasterEntryValue);
+    // const dictCell = Cell.fromBase64(
+    //     'te6cckEBBQEAiAACA8/oAgEAT0gBXkdqCILh0PQ2sSrNftE0+xRx9GR+xJncvd79fC43/OKgO5rKABACASAEAwBNIAU2P6YHTTDT6tly8/WYPdWiPp8tPprjhVSyTGLYU5HWYlloLwBAAE8gAh7pcvxa9Gteci/EiIzOJDTnFyED5Kc5dxjfnHn3j7lCi6Q7dABAuXVYSg=='
+    // );
+    // const dict = dictCell.beginParse().loadDictDirect(Dictionary.Keys.BigUint(256), claimMasterEntryValue);
 
-    const entryIndex = 1n;
+    const entryIndex = 0n;
 
-    const proof = dict.generateMerkleProof(entryIndex);
+    // const proof = dict.generateMerkleProof(entryIndex);
+    const proof = Cell.fromBoc(
+        Buffer.from('b5ee9c724101060100a7000946030dde50f103edae646efec32f69ce442a2834667bf218938bbbbfa20da66137700002012203cfe80302284801014ddf9fa8119499785ef0b30459390756223b2ae064f37f17aa54ce82d3b44f780000220120050428480101593c53c3ea44a6e44bbb1cb21251f5092a0df3ccaef266d06cadf0be0be7230c0000004f20021ee972fc5af46b5e722fc4888cce2434e7172103e4a7397718df9c79f78fb9428ba43b7400404e79ef14', 'hex')
+    )[0]
 
     const helper = provider.open(
         ClaimHelper.createFromConfig(
             {
-                master: Address.parse('EQAckzjUET1SLIEvmsGWRyRVs5szYpRqC8ZV8P8SHfcIl48t'),
+                master: Address.parse('EQDKHmZlHidNgfA3AG0LblmaoihjbP_RIhnRCosd8l4BbP3t'),
                 index: entryIndex,
                 proofHash: proof.hash(),
             },
@@ -40,7 +43,10 @@ export async function run(provider: NetworkProvider) {
     const jettonWalletAddr = await jettonMinter.getWalletAddress(provider.sender().address!);
     const jettonWallet = provider.open(JettonWallet.createFromAddress(jettonWalletAddr));
     console.log('balance before:', await jettonWallet.getJettonBalance());
+
+    // call claim
     await helper.sendClaim(21n, proof); // 123 -> any query_id
+
     const balanceAfter = await waitForStateChange(provider.ui(), async () => await jettonWallet.getJettonBalance());
     console.log('balance after', balanceAfter);
     
